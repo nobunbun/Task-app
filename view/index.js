ae712bad8d7a75ff36f0f52d6d3964511e2ae79d
菊地伸彦 <nobuchin@nobunoPro.local>
2016-12-12 14:24:29 +0900

    firstcommit

--- view/index.js
+++ view/index.js
@@ -1,6 +1,1 @@
-var http = require('http');
-http.createServer(function(req,res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hellow World!!');
-}).listen(3000);
-console.log('Server running at http://localhost:3000/');
+console.log("こんにちは");
